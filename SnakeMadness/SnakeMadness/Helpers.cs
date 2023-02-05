078580c5388f24c6af667863e1ec46b7fcb14dad
Tsvetelin91 <cv.dilov@abv.bg>
2023-02-05 10:32:09 +0200

    Set the startUp snake on the snake field

---
+++ SnakeMadness/SnakeMadness/Helpers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeMadness
+{
+    public class Helpers
+    {
+        public static Queue<Position> StartUpSnake(int numberOFCells)
+        {
+            var snakeElements = new Queue<Position>();
+            for (int i = 0; i < numberOFCells; i++)
+            {
+                snakeElements.Enqueue(new Position(0, i));
+            }
+
+            return snakeElements;
+        }
+
+        public static Position[] Directions()
+        {
+            return new Position[]
+            {
+                new Position(0, 1),
+                new Position(0, -1),
+                new Position(1, 0),
+                new Position(-1, 0),
+            };
+        }
+
+        public static int DirectionSetting(ConsoleKeyInfo userInput)
+        {
+            if (userInput.Key == ConsoleKey.LeftArrow)
+            {
+                return 1;
+            }
+
+            if (userInput.Key == ConsoleKey.RightArrow)
+            {
+                return 0;
+            }
+
+            if (userInput.Key == ConsoleKey.UpArrow)
+            {
+                return 3;
+            }
+
+            if (userInput.Key == ConsoleKey.DownArrow)
+            {
+                return 2;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ SnakeMadness/SnakeMadness/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SnakeMadness;
+using System.Runtime.CompilerServices;
+
+var directions = Helpers.Directions;
+int direction = 0;
+var snakeElements = Helpers.StartUpSnake(5);
+
+while (true)
+{
+    var userInput = Console.ReadKey();
+	direction = Helpers.DirectionSetting(userInput);
+
+    foreach (var position in snakeElements)
+    {
+        Console.SetCursorPosition(position.Col, position.Row);
+        Console.Write("*");
+    }
+}
