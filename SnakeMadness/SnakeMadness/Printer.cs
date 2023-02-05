febc563a1fb12fcf5adab75560aea6884731dbb2
Tsvetelin91 <cv.dilov@abv.bg>
2023-02-05 10:47:37 +0200

    Drawing the snake

---
+++ SnakeMadness/SnakeMadness/Printer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeMadness
+{
+    public class Printer
+    {
+        public static void PrintingTheSnake(Queue<Position> snakeElements)
+        {
+            foreach (var position in snakeElements)
+            {
+                Console.SetCursorPosition(position.Col, position.Row);
+                Console.Write("*");
+            }
+        }
+    }
+}
--- SnakeMadness/SnakeMadness/Program.cs
+++ SnakeMadness/SnakeMadness/Program.cs
@@ -2,18 +2,31 @@
 using SnakeMadness;
 using System.Runtime.CompilerServices;
 
-var directions = Helpers.Directions;
-int direction = 0;
-var snakeElements = Helpers.StartUpSnake(5);
-
-while (true)
+internal class Program
 {
-    var userInput = Console.ReadKey();
-	direction = Helpers.DirectionSetting(userInput);
-
-    foreach (var position in snakeElements)
+    private static void Main(string[] args)
     {
-        Console.SetCursorPosition(position.Col, position.Row);
-        Console.Write("*");
+        var directions = Helpers.Directions();
+        int direction = 0;
+        var snakeElements = Helpers.StartUpSnake(5);
+
+        Printer.PrintingTheSnake(snakeElements);
+
+        while (true)
+        {
+            var userInput = Console.ReadKey();
+            direction = Helpers.DirectionSetting(userInput);
+
+
+            var snakeHead = snakeElements.Last();
+            snakeElements.Dequeue();
+            var nextDirection = directions[direction];
+            var snakeNewHead = new Position(snakeHead.Row + nextDirection.Row,
+                snakeHead.Col + nextDirection.Col);
+            snakeElements.Enqueue(snakeNewHead);
+
+            Console.Clear();
+            Printer.PrintingTheSnake(snakeElements);
+        }
     }
 }
