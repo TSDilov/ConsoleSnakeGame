7e60ae5a186119e719ffd313c4df0e9d8fec2b71
Tsvetelin91 <cv.dilov@abv.bg>
2023-02-05 15:00:52 +0200

    Constructuring the GameEngine

--- SnakeMadness/SnakeMadness/Engine.cs
+++ SnakeMadness/SnakeMadness/Engine.cs
@@ -8,7 +8,21 @@ namespace SnakeMadness
 {
     public class Engine
     {
-        public static bool EndOfGame(Position snakeNewHead, Queue<Position> snakeElements)
+        private readonly Position[] directions;
+        private int direction;
+        private double sleepTime;
+        private Position foodPosition;
+        private readonly Queue<Position> snakeElements;
+
+        public Engine(Position[] directions, ref int direction, ref double sleepTime, ref Position foodPosition, Queue<Position> snakeElements)
+        {
+            this.directions = directions;
+            this.direction = direction;
+            this.sleepTime = sleepTime;
+            this.foodPosition = foodPosition;
+            this.snakeElements = snakeElements;
+        }
+        private static bool EndOfGame(Position snakeNewHead, Queue<Position> snakeElements)
         {
             if (snakeNewHead.Row < 0
                 || snakeNewHead.Col < 0
@@ -24,6 +38,45 @@ namespace SnakeMadness
             }
 
             return false;
+        }
+
+        public void StartGame() 
+        {
+            while (true)
+            {
+                Console.CursorVisible = false;
+                if (Console.KeyAvailable)
+                {
+                    var userInput = Console.ReadKey();
+                    this.direction = Helpers.DirectionSetting(userInput, direction);
+                }
+
+                var snakeHead = snakeElements.Last();
+                var nextDirection = directions[direction];
+                var snakeNewHead = new Position(snakeHead.Row + nextDirection.Row,
+                    snakeHead.Col + nextDirection.Col);
+                if (Engine.EndOfGame(snakeNewHead, snakeElements))
+                {
+                    return;
+                }
+
+                snakeElements.Enqueue(snakeNewHead);
+                Printer.PrintingTheSnake(snakeElements);
+                if (snakeNewHead.Col == foodPosition.Col && snakeNewHead.Row == foodPosition.Row)
+                {
+                    this.foodPosition = Helpers.FoodPosition();
+                    Printer.PrintingTheSnakeFood(foodPosition);
+                    this.sleepTime--;
+                }
+                else
+                {
+                    var last = snakeElements.Dequeue();
+                    Printer.PrintingEmptyElement(last);
+                }
+
+                this.sleepTime -= 0.01;
+                Thread.Sleep((int)this.sleepTime);
+            }
         }
     }
 }
--- SnakeMadness/SnakeMadness/Program.cs
+++ SnakeMadness/SnakeMadness/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SnakeMadness;
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 
 internal class Program
@@ -12,48 +13,10 @@ internal class Program
         var direction = 0;
         double sleepTime = 100;
         var foodPosition = Helpers.FoodPosition();
-
         var snakeElements = Helpers.StartUpSnake(numberOfStartElements);
         Printer.PrintingTheSnakeFood(foodPosition);
         Printer.PrintingTheSnake(snakeElements);
-
-        while (true)
-        {
-            Console.CursorVisible = false;
-            if (Console.KeyAvailable)
-            {
-                var userInput = Console.ReadKey();
-                direction = Helpers.DirectionSetting(userInput, direction);
-            }
-
-            var snakeHead = snakeElements.Last();
-            var nextDirection = directions[direction];
-            var snakeNewHead = new Position(snakeHead.Row + nextDirection.Row,
-                snakeHead.Col + nextDirection.Col);
-            if (Engine.EndOfGame(snakeNewHead, snakeElements))
-            {
-                return;
-            }
-
-            snakeElements.Enqueue(snakeNewHead);
-            Printer.PrintingTheSnake(snakeElements);
-            if (snakeNewHead.Col == foodPosition.Col && snakeNewHead.Row == foodPosition.Row)
-            {
-                foodPosition = Helpers.FoodPosition();
-                Printer.PrintingTheSnakeFood(foodPosition);
-                sleepTime--;
-            }
-            else
-            {
-                var last = snakeElements.Dequeue();
-                Printer.PrintingEmptyElement(last);
-            }
-
-            /*Printer.PrintingTheSnake(snakeElements);
-            Printer.PrintingTheSnakeFood(foodPosition);*/
-
-            sleepTime -= 0.01;
-            Thread.Sleep((int)sleepTime);
-        }
+        var gameEngine = new Engine(directions, ref direction, ref sleepTime, ref foodPosition, snakeElements);
+        gameEngine.StartGame();
     }
 }
