178fac3d96933714054a4a000d915364ab68db02
Tsvetelin91 <cv.dilov@abv.bg>
2023-02-05 11:29:15 +0200

    Changing functionality for eating food

--- SnakeMadness/SnakeMadness/Program.cs
+++ SnakeMadness/SnakeMadness/Program.cs
@@ -22,18 +22,28 @@ internal class Program
             {
                 var userInput = Console.ReadKey();
                 direction = Helpers.DirectionSetting(userInput);
-            }            
+            }
+
 
             var snakeHead = snakeElements.Last();
-            snakeElements.Dequeue();
             var nextDirection = directions[direction];
             var snakeNewHead = new Position(snakeHead.Row + nextDirection.Row,
                 snakeHead.Col + nextDirection.Col);
             snakeElements.Enqueue(snakeNewHead);
+            if (snakeNewHead.Col == foodPosition.Col && snakeNewHead.Row == foodPosition.Row)
+            {
+                foodPosition = Helpers.FoodPosition();
+            }
+            else
+            {
+                snakeElements.Dequeue();
+            }
 
             Console.Clear();
             Printer.PrintingTheSnake(snakeElements);
+
             Printer.PrintingTheSnakeFood(foodPosition);
+
             Thread.Sleep(100);
         }
     }
